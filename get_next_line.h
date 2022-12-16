f29b3a060728728afd2bd604f2774c4da2b7f4f4
cfPurple <97197921+cfPurple@users.noreply.github.com>
2022-12-16 15:29:46 +0100

    g

--- get_next_line.h
+++ get_next_line.h
@@ -12,11 +12,34 @@
 
 #ifndef GET_NEXT_LINE_H
 
-# define GET__NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif 
+
+typedef struct s_list
+{
+	char 			*content;
+	struct s_list	*next;
+}				t_list;
+
 char	*get_next_line(int fd);
+int found_new_line(t_list *stash);
+t_list *ft_lst_get_last(t_list *stash);
+void generate_line(char **line, t_list *stash);
+void free_stash(t_list *stash);
+int ft_strlen(const char *str);
+void read_and_stash(int fd, t_list **stash);
+void add_to_stash(t_list **stash, char *buf, int red);
+void extract_line(t_list *stash, char **line);
+void clean_stash(t_list **stash);
 
 #endif
